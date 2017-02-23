012b7bded84441eebfcdd8c9bef085c2bbd9e142
Vitaliy Makarets <screamaimfire14@gmail.com>
2017-02-23 18:19:08 +0200

    explosion spread

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -4,7 +4,37 @@ using UnityEngine;
 
 public class Bomb : MonoBehaviour {
 
+	private IEnumerator CreateExplosions(Vector3 direction)
+	{
+		//1
+		for (int i = 1; i < 3; i++)
+		{
+			//2
+			RaycastHit hit;
+			//3
+			Physics.Raycast(transform.position + new Vector3(0, .5f, 0), direction, out hit, i, levelMask);
+
+			//4
+			if (!hit.collider)
+			{
+				Instantiate(explosionPrefab, transform.position + (i * direction),
+				  //5 
+				  explosionPrefab.transform.rotation);
+				//6
+			}
+			else
+			{
+				//7
+				break;
+			}
+
+			//8
+			yield return new WaitForSeconds(.05f);
+		}
+	}
+
 	public GameObject explosionPrefab;
+	public LayerMask levelMask;
 
 	void Start () {
 		Invoke("Explode", 3f);
@@ -17,6 +47,11 @@ public class Bomb : MonoBehaviour {
 	void Explode()
 	{
 		Instantiate(explosionPrefab, transform.position, Quaternion.identity); //1
+
+		StartCoroutine(CreateExplosions(Vector3.forward));
+		StartCoroutine(CreateExplosions(Vector3.right));
+		StartCoroutine(CreateExplosions(Vector3.back));
+		StartCoroutine(CreateExplosions(Vector3.left));
 
 		GetComponent<MeshRenderer>().enabled = false; //2
 		transform.FindChild("Collider").gameObject.SetActive(false); //3
