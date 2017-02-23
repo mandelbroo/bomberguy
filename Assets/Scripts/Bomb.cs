c2df4f7e75402737f18414c78d02b72bf7707bbd
Vitaliy Makarets <screamaimfire14@gmail.com>
2017-02-23 18:04:44 +0200

    bomb explosion

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour {
+
+	public GameObject explosionPrefab;
+
+	void Start () {
+		Invoke("Explode", 3f);
+	}
+	
+	void Update () {
+		
+	}
+
+	void Explode()
+	{
+		Instantiate(explosionPrefab, transform.position, Quaternion.identity); //1
+
+		GetComponent<MeshRenderer>().enabled = false; //2
+		transform.FindChild("Collider").gameObject.SetActive(false); //3
+		Destroy(gameObject, .3f); //4
+	}
+}
