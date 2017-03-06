1fd0b2227e9bbc5cca61d5ba6a5227cd2c6b5e32
Vitaliy Makarets <screamaimfire14@gmail.com>
2017-03-06 12:37:53 +0200

    main menu

---
+++ Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+	public bool ShowCursor = true;
+
+	void Start () {
+		Cursor.visible = ShowCursor;
+	}
+	
+	public void LoadScene(String scene)
+	{
+		SceneManager.LoadScene(scene);
+	}
+
+	public void Exit()
+	{
+		Application.Quit();
+	}
+}
